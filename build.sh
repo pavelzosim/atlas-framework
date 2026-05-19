16f78c9cdc24af56e0a62d836da76ba20790a8a8
pavel-zosim <pavel.zosim@protonmail.com>
2026-05-19 15:18:12 +0300

    Update framework

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# 1. Берем только компоненты (они не меняются)
-# 2. Перенаправляем результат в файл, который реально нужен Wix
-cat src/core.css src/typography.css src/components/*.css > atlas-framework.css
-
-# 3. Добавляем мета-информацию (если нужна)
-echo "/* Built on $(date) */" >> atlas-framework.css
-
-echo "Сборка завершена. Файл atlas-framework.css обновлен."
